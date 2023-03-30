6e7712c311a3435bc7feede87265714754b7cfc2
chynel <chynelsoki50@gmail.com>
2023-03-31 00:58:07 +0200

    Ajout de quelques models

--- Clients/models.py
+++ Clients/models.py
@@ -3,8 +3,12 @@
 
 # -- importations des modules -- #
 from django.db import models
+import random
+import string
+import uuid
 from django.utils import timezone
 from Comptes_Utilisateurs.models import CustomUser
+from Services_Offres.models import Service
 
 
 from django.conf import settings
@@ -27,3 +31,35 @@ class Message(models.Model):
     # -- mise en place de la methode __str__ -- #
     def __str__(self):
         return f"Sujet : {self.sujet}" 
+    
+    
+class Facture(models.Model):
+    # -- docstring de la classe Message -- #
+    """
+        -- Cette classe représente la table des messages dans la base de 
+        -- Données
+    """
+    
+    numeroFact = models.CharField(max_length=10, unique=True)
+    totalFact = models.DecimalField(max_digits=10, decimal_places=2)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    etatFact = models.BooleanField(default=False)
+    #numeroResv = models.ForeignKey(Reservation, null = True, on_delete = models.SET_NULL)
+    #numeroCom = models.ForeignKey(Commande, null = True, on_delete = models.SET_NULL)
+    idUser = models.ForeignKey(User, null = True, on_delete = models.SET_NULL)
+
+    def save(self, *args, **kwargs):
+        if not self.numeroFact:
+            self.numeroFact = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        super().save(*args, **kwargs)
+
+
+class Reservation(models.Model):
+    numeroRes = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dateReservation = models.DateField()
+    heureReservation = models.TimeField()
+    idUser = models.ForeignKey(User, null = True, on_delete = models.SET_NULL)
+    idService = models.ForeignKey(Service, null = True, on_delete = models.SET_NULL)
+
+    def __str__(self):
+        return str(self.numeroRes)
