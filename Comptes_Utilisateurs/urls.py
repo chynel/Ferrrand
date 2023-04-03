9b722a573e2e10176dbda2385f1d2da75d5ea4eb
chynel <chynelsoki50@gmail.com>
2023-04-03 05:13:05 +0200

    Definitions des templates

--- Comptes_Utilisateurs/urls.py
+++ Comptes_Utilisateurs/urls.py
@@ -5,6 +5,6 @@ from Comptes_Utilisateurs import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('Enregistrer', views.Enregistrer, name='Enregistrer'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logoout'),
+    path('login', views.connexion, name='connexion'),
+    path('logout', views.logout_view, name='logout_view'),
 ]
--- Comptes_Utilisateurs/views.py
+++ Comptes_Utilisateurs/views.py
@@ -1,21 +1,47 @@
-#coding: utf-8
-from django.http import HttpResponse
-from django.shortcuts import render
+# coding: utf-8
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from Comptes_Utilisateurs.models import CustomUser
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Créez vos vues ici.
 
 
 def home(request):
-    return render (request, 'Comptes_Utilisateurs/index.html')
+    return render(request, 'Comptes_Utilisateurs/index.html')
 
 
 def Enregistrer(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        myCustomUser = CustomUser.objects.create_user(email=email, password=password)
+        messages.success(request, 'Votre compte a été créé avec succès')
+        return redirect('connexion')
+
     return render(request, 'Comptes_Utilisateurs/Enregistrer.html')
 
 
-def login(request):
+def connexion(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        customUser = authenticate(email=email, password=password)
+        if customUser is not None:
+            login(request, customUser)
+            email = customUser.email
+            password = customUser.password
+            print(125)
+            return render(request, 'Comptes_Utilisateurs/index.html', {'email':email})
+        else:
+            messages.error(request, 'Veuillez revoir vos identifiants')
     return render(request, 'Comptes_Utilisateurs/login.html')
 
 
-def logout(request):
-    pass
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Vous avez bien été déconnecté')
+    return redirect('home')
