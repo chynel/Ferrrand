e6e39398887a916c2a4850d4e372e2864ff78307
chynel <chynelsoki50@gmail.com>
2023-04-08 12:18:21 +0200

    Ajout du CSS et JS du fichier login.html

---
+++ Comptes_Utilisateurs/static/js/login1.js
@@ -0,0 +1,10 @@
+const signInBtn = document.getElementById("sign-in");
+const passwordLink = document.getElementById("password-link");
+
+signInBtn.addEventListener("click", () => {
+  // rediriger vers la page d'inscription
+});
+
+passwordLink.addEventListener("click", () => {
+  // rediriger vers la page de modification de mot de passe
+});
