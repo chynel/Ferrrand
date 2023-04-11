a643fb63388c96a85b7b39a8fa759c89b6bb953b
chynel <chynelsoki50@gmail.com>
2023-04-11 14:10:42 +0200

    mm

--- Comptes_Utilisateurs/views.py
+++ Comptes_Utilisateurs/views.py
@@ -197,20 +197,16 @@ def ProfileView(request):
     
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid():
             user_form.save()
-            profile_form.save()
             msg = 'Votre compte a été mis à jour !'
             return redirect('profile')
         else:
             msg = 'Il y a des erreurs dans le formulaire.'
     else:
         user_form = UserUpdateForm(instance=request.user)
-        profile_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
         'user_form': user_form,
-        'profile_form': profile_form
     }
     return render(request, 'profile.html',{"msg": msg, "success": success,})
     
