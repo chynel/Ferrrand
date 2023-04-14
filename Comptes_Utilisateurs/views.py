2c0828c78b66592a4a93ec56dd67909d8c45362e
chynel <chynelsoki50@gmail.com>
2023-04-15 00:13:22 +0200

    Ajustements

--- Comptes_Utilisateurs/views.py
+++ Comptes_Utilisateurs/views.py
@@ -23,84 +23,6 @@ from .models import Profile
 
 
 User = get_user_model()
-"""
-def home(request):
-    if request.user.is_authenticated:
-        email = request.user.email
-        message = f"Vous êtes connecté avec l'adresse : {email}"
-        button_label = "Se deconnecter"
-        button_url = "logout_view"
-    else:
-        message = "Vous n'êtes plus connecté"
-        button_label = ""
-        button_url = ""
-    return render(request, 'index.html', {'message': message, 'button_label': button_label, 'button_url': button_url})
-
-
-
-def Enregistrer(request):
-    if request.method == "POST":
-        email = request.POST['email']
-        password = request.POST['password']
-        password1 = request.POST['password1']
-        
-        if password == password1:
-            if User.objects.filter(email=email):
-                messages.info(request, "L'email entrez existe déjà")
-                return redirect('Enregistrer')
-            else:
-                user = User.objects.create_user(email=email, password=password)
-                user.set_password(password)
-                user.save()
-                messages.success(request, 'Votre compte a été créé avec succès')
-                return redirect('login')
-        else:
-            messages.info(request, "Les mots de passe sont différents")
-
-        
-
-    return render(request, 'Enregistrer.html')
-
-
-def login(request):
-    if request.method == "POST":
-        email = request.POST['email']
-        password = request.POST['password']
-        user = auth.authenticate(email=email, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Veuillez revoir vos identifiants')
-    return render(request, 'login.html')
-
-
-def logout_view(request):
-    auth.logout(request)
-    messages.success(request, 'Vous avez bien été déconnecté')
-    return redirect('home')
-
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, 'Votre compte a été mis à jour !')
-            return redirect('profile')
-    else:
-        user_form = UserUpdateForm(instance=request.user)
-        profile_form = ProfileUpdateForm(instance=request.user.profile)
-    context = {
-        'user_form': user_form,
-        'profile_form': profile_form
-    }
-    return render(request, 'profile.html', context)
-    
-"""
 
 #Juste pour voir
 
@@ -242,11 +164,8 @@ def ProfileUpdateView(request):
 
 
 
+
 def changpassword(request):
-    
-    msg = None
-    success = False
-    
     if request.method == 'POST':
         email = request.POST.get('email')
         question = request.POST.get('question')
@@ -258,28 +177,27 @@ def changpassword(request):
         try:
             user = User.objects.get(email=email)
         except:
-            msg = "Le mail entré n'existe pas. Veuillez réessayer."
+            messages.error(request, "Le mail entré n'existe pas. Veuillez réessayer.")
             return redirect('changpassword')
 
         # Vérifier si la réponse à la question secrète est correcte
         
         if user.question != question or user.reponse != reponse:
-            msg = 'La réponse à la question secrète est incorrecte.'
-            return redirect('changpassword')
+            messages.error(request, "La question ou la réponse à la question secrète est incorrecte.")
 
         # Vérifier si les deux mots de passe correspondent
         if password1 != password2:
-            msg = 'Les deux mots de passe ne correspondent pas.'
-            return redirect('changpassword')
+            messages.error(request, 'Les deux mots de passe ne correspondent pas.')
 
         # Réinitialiser le mot de passe
-        user.set_password(password1)
-        user.save()
-        
-        msg = 'Le mot de passe a été réinitialisé avec succès.'
-        success = True
-            
+        if not messages.get_messages(request):
+            user.set_password(password1)
+            user.save()
+            messages.success(request, 'Le mot de passe a été réinitialisé avec succès.')
 
     # Afficher le formulaire de réinitialisation du mot de passe
     question_choices = User.QUESTION_CHOICES
-    return render(request, 'changpassword.html', {'question_choices': question_choices, "msg": msg, "success": success,})
+    context = {'question_choices': question_choices}
+    for message in messages.get_messages(request):
+        context[str(message.level_tag)] = message
+    return render(request, 'changpassword.html', context)
